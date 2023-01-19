dcb6808b65a4f0d5712e322f70906198ca455f2e
Lumunix <lumunix@icloud.com>
2023-01-19 13:41:55 -0500

    adding

---
+++ pytest_bdd_ui_automation/DriverFactory.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+class DriverFactory:
+    def get_driver(self, config) -> WebDriver:
+
+        if config['browser'] == 'firefox':
+            firefox_options = webdriver.FirefoxOptions()
+            driver = webdriver.Remote(
+                options=self.set_options(config, firefox_options),
+                command_executor=config['selenium_hub_url'])
+        elif config['browser'] == 'chrome':
+            chrome_options = webdriver.ChromeOptions()
+            driver = webdriver.Remote(
+                options=self.set_options(config, chrome_options),
+                command_executor=config['selenium_hub_url'])
+        else:
+            raise Exception(f'Browser "{config["browser"]}" is not supported')
+
+        return driver
+
+    @staticmethod
+    def set_options(config, options):
+        options.add_argument('start-maximized')
+        options.add_argument('window-size=1920x1080')
+        options.set_capability("se:recordVideo", "true")
+        options.set_capability("se:screenResolution", "1920x1080")
+        if config['headless']:
+            options.add_argument('headless')
+
+        return options
---
+++ pytest_bdd_ui_automation/conftest.py
@@ -0,0 +1,168 @@
+import logging
+import os
+import pytest
+import json
+from datetime import datetime
+from selenium.webdriver import Remote
+from alert_console_tests.DriverFactory import DriverFactory
+from alert_rest_services.utils.AuthConfig import UtilsClass
+
+from alert_rest_services.utils.AuthConfig import UtilsClass
+from alert_rest_services.services.grpc_service.Coreservice.BaseTestclass import Basetest
+from alert_rest_services.services.grpc_service.HelperMethods.HelperMethodMixin import HelperMethod
+
+DEFAULT_CONFIG_PATH = 'config.json'
+pytest_plugins = [
+    "alert_console_tests.steps.common",
+    "alert_console_tests.steps.grpc"]
+
+
+def pytest_addoption(parser):
+    """ Parse pytest --option variables from shell """
+    parser.addoption('--config', help='Path to the configuration file to use',
+                     default=DEFAULT_CONFIG_PATH)
+
+
+@pytest.fixture
+def config_arg(request):
+    """ :returns Config path from --config option """
+    return request.config.getoption('--config')
+
+
+@pytest.fixture
+def url():
+    namespace = os.environ.get('NAMESPACE')
+    kubernetes_cluster = os.environ.get('KUBERNETES_CLUSTER')
+    if not namespace:
+        raise Exception(f'NAMESPACE environment variable not set, '
+                        f'set using export NAMESPACE=YOUR_NAMESPACE or in ~/bash_profile.')
+    if not kubernetes_cluster:
+        raise Exception(f'KUBERNETES_CLUSTER environment variable not set, '
+                        f'set using export KUBERNETES_CLUSTER=CLUSTER_NAME or in ~/bash_profile.')
+
+    return f'http://{namespace}.{kubernetes_cluster}.do-alert-innovation.com/{namespace}/acs/console'
+
+
+@pytest.fixture
+def config(config_arg):
+    browsers = ['chrome', 'firefox']
+
+    # Read config file
+    logging.info(f'Config File: {config_arg}')
+
+    with open(os.path.join(os.path.dirname(__file__),
+                           config_arg)) as config_file:
+        config = json.load(config_file)
+
+    # Assert values are acceptable
+    assert isinstance(config['selenium_hub_url'], str)
+    assert config['selenium_hub_url']
+    assert config['browser'] in browsers
+    assert isinstance(config['headless'], bool)
+    assert isinstance(config['implicit_wait'], int)
+    assert config['implicit_wait'] > 0
+
+    # Return config so it can be used
+    return config
+
+
+@pytest.fixture
+def browser(config, url) -> Remote:
+    """ Select configuration depends on browser and host """
+    # Configure browser and driver
+    driver = DriverFactory().get_driver(config)
+
+    # Make call wait up to 10 seconds for elements to appear
+    driver.implicitly_wait(config['implicit_wait'])
+
+    logging.info(f"BROWSER RESOLUTION: {str(driver.get_window_size())}")
+    logging.info(f'URL: {url}')
+
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture
+def pages(url):
+    return {
+        "login": f'{url}' + "/login",
+        "bots": f'{url}' + "/home/system/bots",
+        "totes": f'{url}' + "/home/system/totes",
+        "workstations": f'{url}' + "/home/system/workstations",
+        "safety": f'{url}' + "/home/system/safety",
+        "activity": f'{url}' + "/home/system/activity",
+        "user areas": f'{url}' + "/home/system/userAreas",
+        "configs": f'{url}' + "/home/system/configs",
+        "user actions": f'{url}' + "/home/userActions",
+        "bot cycles": f'{url}' + "/home/testing/botCycles"
+    }
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+    if report.when == 'call':
+        feature_request = item.funcargs['request']
+        driver = feature_request.getfixturevalue('browser')
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            timestamp = datetime.now().strftime('%m-%d-%Y:%H-%M-%S')
+            screenshot_path = report.head_line + ':' + timestamp + '.png'
+            driver.save_screenshot(screenshot_path)
+            extra.append(pytest_html.extras.url(driver.current_url))
+            extra.append(pytest_html.extras.image(screenshot_path))
+        report.extra = extra
+
+
+
+# GRPC FIXTURES
+def pytest_configure():
+    UtilsClass.util_initialize()
+    pytest.authkey = UtilsClass.request_auth_token()
+    pytest.pathname = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources",
+                                   "testdata")
+
+
+@pytest.fixture
+def grpc_core():
+    """
+    Fixture method to provided the grpcs channels for all the services. With this object, you can access all the
+    service channel
+    """
+    grpc_core = Basetest.get_instance()
+    UtilsClass.util_initialize()
+    pytest.authkey = UtilsClass.request_auth_token()
+    return grpc_core
+
+
+@pytest.fixture()
+def helper_method():
+    """
+    fixture methods to retrieve the helpermethods for all the services
+    """
+    helper_method = HelperMethod()
+    return helper_method
+
+
+@pytest.fixture
+def grpc_core():
+    """
+    Fixture method to provided the grpcs channels for all the services. With this object, you can access all the
+    service channel
+    """
+    grpc_core = Basetest.get_instance()
+    UtilsClass.util_initialize()
+    pytest.authkey = UtilsClass.request_auth_token()
+    return grpc_core
+
+
+@pytest.fixture()
+def helper_method():
+    """
+    fixture methods to retrieve the helpermethods for all the services
+    """
+    helper_method = HelperMethod()
+    return helper_method
+
